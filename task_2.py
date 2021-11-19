b8fa0c98bd83f2419242a58343a1e6659efb66c5
Artem Davydov <90029083+ArtemDavydov12@users.noreply.github.com>
2021-11-19 11:03:51 +0300

    Create task_2.py

---
+++ task_2.py
@@ -0,0 +1,13 @@
+t = [[2, 4, 6], [1, 3, 5], [7, 8, 9]]
+c = 0
+cs = [[], [], []]
+s = 0
+for i in t:
+    l = 0
+    for k in i:
+        c += k
+        cs[s].append(c)
+        l += 1
+        if l == 3:
+            s += 1
+print(cs)
