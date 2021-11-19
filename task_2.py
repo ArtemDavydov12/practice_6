cc823607cd15930197d28f0ee7df9d34c67fa3c6
Artem Davydov <90029083+ArtemDavydov12@users.noreply.github.com>
2021-11-19 11:26:23 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,13 +1,17 @@
+def cumsum(t):
+    c = 0
+    cs = [[], [], []]
+    s = 0
+    for i in t:
+        l = 0
+        for k in i:
+            c += k
+            cs[s].append(c)
+            l += 1
+            if l == 3:
+                s += 1
+    return cs
+
+
 t = [[2, 4, 6], [1, 3, 5], [7, 8, 9]]
-c = 0
-cs = [[], [], []]
-s = 0
-for i in t:
-    l = 0
-    for k in i:
-        c += k
-        cs[s].append(c)
-        l += 1
-        if l == 3:
-            s += 1
-print(cs)
+print(cumsum(t))
