2f870cba2e43ccf49bf6a1b839ca865501af68e7
Artem Davydov <90029083+ArtemDavydov12@users.noreply.github.com>
2021-11-19 11:24:13 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,9 @@
+def middle(t):
+  t.pop(0)
+  t.reverse()
+  t.pop(0)
+  t.reverse()
+  return t
+
 t = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-t.pop(0)
-t.reverse()
-t.pop(0)
-t.reverse()
-print(t)
+print(middle(t))
